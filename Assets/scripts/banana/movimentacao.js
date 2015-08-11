fab8f292fcb873d90b17901d4fb44a7a489fe0c3
leandro <leandro.xse@gmail.com>
2015-08-10 23:17:01 -0300

    Modificações finais 10/08. Pronto pra apresentar.

--- Assets/scripts/banana/movimentacao.js
+++ Assets/scripts/banana/movimentacao.js
@@ -9,16 +9,16 @@ var andaresquerda: boolean;
 var velocidade: float;
 
 function Start () {
-	velocidade = 17*Time.deltaTime;
+	velocidade = 10*Time.deltaTime;
 	
 }
 
 function Update (){
-
-	andarfrente = Input.GetKey("w");
-	andartras = Input.GetKey("s");
-	andardireita = Input.GetKey("d");
-	andaresquerda = Input.GetKey("a");
+	
+	andarfrente = Input.GetKey(KeyCode.UpArrow);
+	andartras = Input.GetKey(KeyCode.DownArrow);
+	andardireita = Input.GetKey(KeyCode.RightArrow);
+	andaresquerda = Input.GetKey(KeyCode.LeftArrow);
 
 	if(andarfrente == true){
 		transform.Translate(0,0,velocidade);
